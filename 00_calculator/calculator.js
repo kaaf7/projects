9ffe09e7817fc0a1b5bd80f28538e560afdab2a2
kaaf7 <amer.m.karim@gmail.com>
2022-08-07 17:59:34 +0200

    init

--- 00_calculator/calculator.js
+++ 00_calculator/calculator.js
@@ -1,10 +1,39 @@
+let screenInput = document.querySelector(".calculator-numbers");
+const buttonNumbers = document.querySelectorAll(".btn-");
+
+let stepA;
+let stepB;
+let stepC;
+
+for (let i = 0; i < buttonNumbers.length; i++) {
+  buttonNumbers[i].addEventListener("click", () => {
+    var elementValue = buttonNumbers[i].value;
+
+    if (screenInput.value === "0") {
+      screenInput.value = elementValue;
+    } else {
+      screenInput.value += elementValue;
+    }
+  });
+}
 function add(a, b) {
   a + b;
   return add;
 }
 
 function subtract(a, b) {
-    a - b;
-    return subtract;
-  }
-  
+  a - b;
+  return subtract;
+}
+
+function divide(a, b) {
+  a / b;
+  return divide;
+}
+
+function multiply(a, b) {
+  a / b;
+  return multiply;
+}
+
+function Operate(a, b, operation) {}
