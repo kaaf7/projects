a900b2cd21b9fcc2b6acfda17cd8bdb5b79f1e1a
kaaf7 <amer.m.karim@gmail.com>
2022-08-08 16:15:50 +0200

    init

--- 00_calculator/calculator.js
+++ 00_calculator/calculator.js
@@ -1,13 +1,16 @@
 const screenInput = document.querySelector(".calculator-numbers");
-const buttonNumbers = document.querySelectorAll(".btn-");
-const addButton = document.getElementById("add");
-const operationButton = document.querySelectorAll(".btn-blue-operator");
+const buttonNumbers = document.querySelectorAll(".btn-"); // selecting all the number buttons so i can loop through them
+const operationButton = document.querySelectorAll(".btn-blue-operator"); // slecting all the math operatio buttons so i can loop through them
 
 // this is an empty variable to save the data inside while doing the math operations
 var input = "0";
 
 // this is clear button which will clear the screen from the numbers on it
 const clearButton = document.getElementById("clear-operator");
+
+function operation(a, b, operate) {
+  parseInt(a) + operate + parseInt(b);
+}
 
 // this is a loop to get all the numbers and then add them into the screen as a a string value which will be converted later into int for the math operation
 for (let i = 0; i < buttonNumbers.length; i++) {
@@ -21,12 +24,7 @@ for (let i = 0; i < buttonNumbers.length; i++) {
   });
 }
 
-// this is for operation buttons activation + - x / =
-for (let j = 0; j <= operationButton.length; j++) {
-  operationButton[j]?.addEventListener("click", () => {
-    console.log(operationButton[j]?.id + " is executed ");
-  });
-}
+// this is for math operation buttons activation + - x / =
 
 // this is for clear button to clear the screen from the numbers on it
 clearButton.addEventListener("click", () => {
@@ -34,26 +32,34 @@ clearButton.addEventListener("click", () => {
     input = screenInput.value; // storing the data into other variable so we can use later for math operations
     console.log(input);
     screenInput.value = ""; // turning input value into 0
-    console.log(add(parseInt(input), parseInt(input))); // for parsing values from string into integer for mathematical operations
   } else {
     screenInput.value = ""; // turniing screen value into 0
   }
 });
 
-function add(a, b) {
-  return a + b;
+for (let j = 0; j <= operationButton.length; j++) {
+  operationButton[j]?.addEventListener("click", () => {
+    console.log(window[operationButton[j]?.id](screenInput.value,screenInput.value)); // window["string"] this turns string from the id into function so we can save time by executing id which executes the function whith similar name to the iud
+  });
+}
+
+// the basic math operations
+
+function sum(a, b) {
+  
+  return parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {
-  return a - b;
+  return parseInt(a) - parseInt(b);
 }
 
 function divide(a, b) {
-  return a / b;
+  return parseInt(a) / parseInt(b);
 }
 
 function multiply(a, b) {
-  return a * b;
+  return parseInt(a) * parseInt(b);
 }
 
 /// a trial way to see if i can combine all the operator buttons into one grid and then i found a better way to summerize it in less than 5 lines of code instead of 50
