fd5cd3b08c4651a950dc18feb5ba1a730b2d162c
kaaf7 <amer.m.karim@gmail.com>
2022-08-08 13:32:37 +0200

    init

--- 00_calculator/calculator.js
+++ 00_calculator/calculator.js
@@ -1,9 +1,11 @@
 const screenInput = document.querySelector(".calculator-numbers");
 const buttonNumbers = document.querySelectorAll(".btn-");
 const addButton = document.getElementById("add");
+const operationButton = document.querySelectorAll(".btn-blue-operator");
+
 var input = "0";
 
-const clearButton = document.getElementById("clear");
+const clearButton = document.getElementById("clear-operator");
 
 for (let i = 0; i < buttonNumbers.length; i++) {
   buttonNumbers[i].addEventListener("click", () => {
@@ -17,15 +19,22 @@ for (let i = 0; i < buttonNumbers.length; i++) {
   });
 }
 
+for (let j = 0; j <= operationButton.length; j++) {
+  if (operationButton[j].id === "add-operator") {
+    operationButton[j].addEventListener("click", () => {
+      console.log("add is activated");
+    });
+  }
+}
+
 clearButton.addEventListener("click", () => {
   if (screenInput.value !== "0") {
     input = screenInput.value; // storing the data into other variable so we can use later for math operations
     console.log(input);
-    screenInput.value = ""; // turning input value into 0 
-    console.log( add(parseInt(input),parseInt(input))); // for parsing values from string into integer for mathematical operations
-
+    screenInput.value = ""; // turning input value into 0
+    console.log(add(parseInt(input), parseInt(input))); // for parsing values from string into integer for mathematical operations
   } else {
-    screenInput.value = "";
+    screenInput.value = ""; // turniing screen value into 0
   }
 });
 
@@ -42,12 +51,9 @@ function subtract(a, b) {
 }
 
 function divide(a, b) {
-  return a/ b;
+  return a / b;
 }
 
 function multiply(a, b) {
   return a * b;
 }
-
-
-
