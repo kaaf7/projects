4d9f7a4afc820717681b2d5fa2fa920f4aef7809
kaaf7 <amer.m.karim@gmail.com>
2022-08-11 13:36:15 +0200

    init

--- 00_calculator/calculator.js
+++ 00_calculator/calculator.js
@@ -32,17 +32,18 @@ zeroButton.addEventListener("click", (e) => {
   handleNumber(e.target.textContent);
 });
 
-function handleOperator(op) {
-  operator = op;
-  previousNum = currentNum;
-  screenDisplay.textContent = ""; // set the display back to nothing
-  currentNum = ""; // set the current number back to nothing
-}
 function handleNumber(number) {
   if (currentNum.length <= 10) {
     currentNum += number;
     screenDisplay.textContent = currentNum;
   }
+}
+
+function handleOperator(op) {
+  operator = op;
+  previousNum = currentNum;
+  screenDisplay.textContent = ""; // set the display back to nothing
+  currentNum = ""; // set the current number back to nothing
 }
 
 function handleMath() {
@@ -53,14 +54,16 @@ function handleMath() {
       currentNum += previousNum;
     } else if (operator === "-") {
       currentNum = previousNum - currentNum;
-    } else if (operator === "/") {
-      currentNum = previousNum / currentNum;
     } else if (operator === "x") {
       currentNum = previousNum * currentNum;
+    } else if (operator === "/") {
+      if (currentNum <= 0) {
+        screenDisplay.textContent = "Error";
+        return;
+      }
+      currentNum = previousNum / currentNum;
     }
-    if (currentNum.length > 10) {
-      currentNum = currentNum.slice(0, 10);
-    }
+
     screenDisplay.textContent = currentNum;
     previousNum = ""; // to stop the equal button from repeating the equation everytime we press the equal button
   }
@@ -83,164 +86,11 @@ bStepButton.addEventListener("click", () => {
   }
 });
 
-// // this is an empty variable to save the data inside while doing the math operations
-// var emptyInput = "0";
-
-// function operation(a, b, operate) {
-//   parseInt(a) + operate + parseInt(b);
-// }
-
-// // this is a loop to get all the numbers and then add them into the screen as a astring value which will be converted later into int for the math operation
-// // for (let i = 0; i < buttonNumbers.length; i++) {
-// //   buttonNumbers[i]?.addEventListener("click", () => {
-// //     var elementValue = buttonNumbers[i].value;
-// //     if (screenInput.value === "0") {
-// //       screenInput.value = elementValue;
-// //     } else {
-// //       screenInput.value += elementValue;
-// //     }
-// //   });
-// // }
-
-// //this is a loop to get all the numbers and then add them into the screen as a astring value which will be converted later into int for the math operation
-
-// buttonNumbers.forEach((btn) => {
-//   btn.addEventListener("click", (e) => {
-//     handleNumber(e.target.textContent);
-//   });
-// });
-
-// operationButton.forEach((btn) => {
-//   btn.addEventListener("click", () => {
-//     handleOperation(e.target.textConten);
-//   });
-// });
-
-// function handleOperation(op) {
-//   if (previousNum === "") {
-//     previousNum = currentNum;
-//     operatorCheck(op)
-//   } else if ( currentNum ===""){
-//     operatorCheck(op)
-//   }
-// }
-
-// function operatorCheck(text) {
-//   operator = text;
-//   screenDisplay.textContent = previousNum + " " + operator;
-//   currentNum = "";
-// }
-
-// function handleNumber(number) {
-//   if (currentNum.length <= 10) {
-//     currentNum += number;
-//     screenDisplay.textContent = currentNum;
-//   }
-// }
-
-// // this is for clear button to clear the screen from the numbers in it
-// function clearOperation() {
-//   screenInput.value = "0"; // turniing screen value into 0
-// }
-
-// clearButton.addEventListener("click", clearOperation);
-
-// // //this is for math operation buttons activation + - x / =
-// // for (let j = 0; j <= operationButton.length; j++) {
-// //   operationButton[j]?.addEventListener("click", () => {
-// //     console.log(operationButton[j]?.id);
-// //     //console.log(window[operationButton[j]?.id](input, input)); // window["string"] => window [sum] this turns string from the id into function so we can save time by executing id which executes the function whith similar name to the id
-// //   });
-// // }
-
-// // for (let j = 0; j <= operationButton.length; j++) {
-// //   operationButton[j]?.addEventListener("click", () => {});
-// // }
-
-// // the basic math operations
-// function sum(a, b) {
-//   return parseInt(a) + parseInt(b);
-// }
-
-// function subtract(a, b) {
-//   return parseInt(a) - parseInt(b);
-// }
-
-// function divide(a, b) {
-//   return parseInt(a) / parseInt(b);
-// }
-
-// function multiply(a, b) {
-//   return parseInt(a) * parseInt(b);
-// }
-
-// // for (j = 0; j <= operationButton.length; j++) {
-// //   operationButton[j]?.addEventListener("click", () => {
-// //     emptyInput = screenInput.value;
-// //     screenInput.value = "";
-// //     equalButtons.addEventListener("click", () => {
-// //       screenInput.value = sum(
-// //         parseInt((screenInput.value /*+= emptyInput*/)),
-// //         parseInt(emptyInput)
-// //       );
-// //     });
-// //   });
-// // }
-
-// // for (j = 0; j <= operationButton.length; j++) {
-// //   operationButton[j]?.addEventListener("click", () => {
-// //     emptyInput = screenInput.value;
-// //     screenInput.value = "";
-
-// //     equalButtons.addEventListener("click", () => {
-// //       screenInput.value = emptyInput + screenInput.value;
-// //     });
-// //   });
-// // }
-
-// sumBtn.addEventListener("click", () => {
-//   emptyInput = screenInput.value;
-//   screenInput.value = "";
-//   equalButtons.addEventListener("click", () => {
-//     screenInput.value += emptyInput;
-//   });
-// });
-
-// // //a trial way to see if i can combine all the operator buttons into one grid and then i found a better way to summerize it in less than 5 lines of code instead of 50
-// // for (let j = 0; j <= operationButton.length; j++) {
-// //   if (operationButton[j]?.id === "sum") {
-// //     operationButton[j]?.addEventListener("click", () => {
-// //       console.log("add is activated");
-// //       return;
-// //     });
-// //   } else if (operationButton[j]?.id === "subtract") {
-// //     operationButton[j]?.addEventListener("click", () => {
-// //       clearOperation;
-// //       return true;
-// //     });
-// //   } else if (operationButton[j]?.id === "divide") {
-// //     operationButton[j]?.addEventListener("click", () => {
-// //       console.log("divide is activated");
-// //       return;
-// //     });
-// //   } else if (operationButton[j]?.id === "multiply") {
-// //     operationButton[j]?.addEventListener("click", () => {
-// //       console.log("multiply is activated");
-// //       emptyInput = screenInput.value;
-// //       screenInput.value = screenInput.value;
-// //       clearOperation();
-// //       if (operationButton[j]?.id === "equal") {
-// //         operationButton[j]?.addEventListener("click", () => {
-// //           clearOperation();
-// //           var newValue = toString(multiply("5", "5"));
-// //           screenInput.value = newValue;
-// //         });
-// //       }
-// //     });
-// //   } else if (operationButton[j]?.id === "equal-operator") {
-// //     operationButton[j]?.addEventListener("click", () => {
-// //       console.log("equal is activated");
-// //       return;
-// //     });
-// //   }
-// // }
+function round(num) {
+  if (num.length > 10) {
+    num = num.substring(0, 10);
+  }
+}
+function roundNumber(num) {
+  return Math.round(num * 100000) / 100000;
+}
