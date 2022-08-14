cb8a344d60e0f18c5fe9201c0ec5760e0b79e561
kaaf7 <amer.m.karim@gmail.com>
2022-08-14 18:42:10 +0200

    init

---
+++ 01_library/library.js
@@ -0,0 +1,7 @@
+const addButton = document.querySelector('.add-btn')
+const bookForm = document.querySelector('.fill-form')
+
+addButton.addEventListener('click', ()=>{
+
+bookForm.style.display = "none"
+})
