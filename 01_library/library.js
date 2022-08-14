e77c3cb6436693c0b67b44d1faf26b2e08bbf51a
kaaf7 <amer.m.karim@gmail.com>
2022-08-14 15:49:49 +0200

    init

--- 01_library/library.js
+++
@@ -1,8 +0,0 @@
-let bookArr = document.querySelector("book-arr");
-let bookContainer = document.querySelector("container");
-
-let myLibrary = [];
-let newBook = { genre: "", Title: "", Author: "", Year: "", type: "" };
-function book() {}
-function addBookToLibrary() {}
-
