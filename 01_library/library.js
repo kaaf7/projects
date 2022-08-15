dc9b551719a6414e3f587dbaa238928865d0d087
kaaf7 <amer.m.karim@gmail.com>
2022-08-15 11:35:36 +0200

    init

--- 01_library/library.js
+++ 01_library/library.js
@@ -1,10 +1,10 @@
 const addButton = document.querySelector(".add-btn");
 
 const bookName = document.querySelectorAll("book-name");
+
 const bookAthor = document.getElementById("book-author");
+
 const bookYear = document.getElementById("book-year");
+
 const bookGenre = document.getElementbyId("book-genre");
 //const readOrNot;
-
-
-
