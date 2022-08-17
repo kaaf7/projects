1997ccfa89209fd3b67a7c1856b3260d5efa39cd
kaaf7 <amer.m.karim@gmail.com>
2022-08-17 16:26:08 +0200

    init

--- 001_library_/nLibrary.js
+++ 001_library_/nLibrary.js
@@ -11,6 +11,18 @@ let myLibrary = [
     year: "",
     genre: "",
   },
+  {
+    name: "",
+    author: "",
+    year: "",
+    genre: "",
+  },
+  {
+    name: "",
+    author: "",
+    year: "",
+    genre: "",
+  },
 ];
 let bookObj = { bookName: "", AuthorName: "", bookYear: "", bookGenre: "" };
 
@@ -21,18 +33,26 @@ board.style.gridTemplateRows = `repeat(${myLibrary.length},1fr)`;
 function createLibrary(library) {
   for (let i = 0; i <= library.length; i++) {
     let card = document.createElement("section");
-    card.style.backgroundColor = "grey";
     let bookName = document.createElement("div");
     let bookAuthor = document.createElement("div");
+    let bookYear = document.createElement("div");
+    let bookGenre = document.createElement("div");
+    // let checkBox = document.createElement("INPUT");
+    // let checkBoxLabel = document.createElement("label")
+    //checkBox.setAttribute("type", "checkbox");
     bookName.innerHTML = "book Name";
     bookAuthor.innerHTML = "book Author";
+    bookYear.innerHTML = "Book Year";
+    bookGenre.innerHTML = "Book Genre";
+    // checkBoxLabel.innerHTML = "Read?"
     card.appendChild(bookName);
     card.appendChild(bookAuthor);
-
+    card.appendChild(bookYear);
+    card.appendChild(bookGenre);
+    // card.appendChild(checkBox);
+    // card.appendChild(checkBoxLabel);
     board.insertAdjacentElement("beforeend", card);
   }
 }
 
 createLibrary(myLibrary);
-
-function createBook(book) {}
