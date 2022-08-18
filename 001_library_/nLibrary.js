ca6e3b93732c0d819acd6a18a42e22d0ffa003c0
kaaf7 <amer.m.karim@gmail.com>
2022-08-18 18:21:35 +0200

    init

--- 001_library_/nLibrary.js
+++ 001_library_/nLibrary.js
@@ -1,69 +1,96 @@
 const addButton = document.querySelector(".add-btn");
+
 const closeButton = document.querySelector(".close-btn");
+
 const submitButton = document.querySelector(".sumbit-btn");
+
 const formElements = document.querySelector(".form__input-group");
+
+const formLabel = document.querySelector(".form__input-label");
+
 const bookNameInput = document.getElementById("bookName");
+
 const bookAuthorInput = document.getElementById("bookAuthor");
+
 const bookYearInput = document.getElementById("bookYear");
+
 const bookGenreInput = document.getElementById("bookGenre");
 
+const fillForm = document.querySelector(".form");
+
 let board = document.querySelector(".board");
+
 var bookObj = { bookName: "", AuthorName: "", bookYear: "", bookGenre: "" };
 
-let myLibrary = JSON.parse(localStorage.getItem("books")) || [];
+let myLibrary = JSON.parse(localStorage.getItem("books")) || []; // parse file from localhost
 
 board.style.gridTemplateColumns = "repeat(5,1fr)";
 board.style.gridTemplateRows = `repeat(${myLibrary.length},1fr)`;
 
 addButton.addEventListener("click", () => {
-  const fillForm = document.querySelector(".form");
-  if (fillForm.style.visibility === "hidden") {
-    fillForm.style.visibility = "visible";
-  } else {
-    fillForm.style.visibility = "hidden";
-    formElements.style.visibility === "hidden";
-  }
+  fillForm.style.visibility = "visible";
 });
+
+function closeForm(form) {
+  form.style.visibility = "hidden";
+}
 
 closeButton.addEventListener("click", () => {
-  if (fillForm.style.visibility === "visible") {
-    fillForm.style.visibility = "hidden";
+  fillForm.style.visibility = "hidden";
+});
+
+submitButton.addEventListener("click", CreateBook);
+
+function CreateBook() {
+  if (
+    bookNameInput.value !== "" &&
+    bookAuthorInput.value !== "" &&
+    bookYearInput.value !== "" &&
+    bookGenreInput.value !== ""
+  ) {
+    bookObj.bookName = bookNameInput.value;
+    bookObj.AuthorName = bookAuthorInput.value;
+    bookObj.bookYear = bookYearInput.value;
+    bookObj.bookGenre = bookGenreInput.value;
+    myLibrary.push(bookObj);
+    localStorage.setItem("books", JSON.stringify(myLibrary)); // store file in localhost
+    createLibraryCard(myLibrary);
+    closeForm(fillForm);
+  } else {
+    alert("please insert  info");
   }
-});
-
-submitButton.addEventListener("click", () => {
-  bookObj.bookName = bookNameInput.value;
-  bookObj.AuthorName = bookAuthorInput.value;
-  bookObj.bookYear = bookYearInput.value;
-  bookObj.bookGenre = bookGenreInput.value;
-  myLibrary.push(bookObj);
-  localStorage.setItem("books", JSON.stringify(myLibrary));
-  createLibrary(myLibrary);
-  bookObj.bookName = "";
-  bookObj.AuthorName = "";
-  bookObj.bookYear = "";
-  bookObj.bookGenre = "";
   return;
-});
+}
 
-function CreateBook() {}
-
-function createLibrary(library) {
+function createLibraryCard(library) {
   for (let i = 0; i < library.length; i++) {
-    library = library[i]; // to add one extra book at a time
+    library = library[i]; // to add one book at a time
     let card = document.createElement("section");
+    let btn = document.createElement("button");
     let bookName = document.createElement("div");
     let bookAuthor = document.createElement("div");
     let bookYear = document.createElement("div");
     let bookGenre = document.createElement("div");
-    bookName.innerHTML = "book Name";
-    bookAuthor.innerHTML = "book Author";
-    bookYear.innerHTML = "Book Year";
-    bookGenre.innerHTML = "Book Genre";
+    bookName.innerHTML = "Book: " + bookObj.bookName;
+    bookAuthor.innerHTML = "Author:" + bookObj.AuthorName;
+    bookYear.innerHTML = "Year: " + bookObj.bookYear;
+    bookGenre.innerHTML = "Genre: " + bookObj.bookGenre;
+    btn.style.width = "200px";
+    btn.style.height = "30px";
+    btn.innerHTML = "X";
+    btn.style.border = "none";
+    btn.style.background = "#F4D19B";
+    btn.style.fontFamily = "Bebas Neue";
+    btn.style.borderRadius ="5px" 
     card.appendChild(bookName);
     card.appendChild(bookAuthor);
     card.appendChild(bookYear);
     card.appendChild(bookGenre);
+    card.appendChild(btn);
     board.insertAdjacentElement("beforeend", card);
+    btn.addEventListener("click", () => {
+      var ind = library.findIndex(card);
+      console.log(ind);
+    });
   }
 }
