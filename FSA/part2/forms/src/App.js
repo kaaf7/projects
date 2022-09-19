f1e4ddeb9aca9b318124ae60b5577179b303bfc9
kaaf7 <amer.m.karim@gmail.com>
2022-09-19 17:13:37 +0200

    init

--- FSA/part2/forms/src/App.js
+++ FSA/part2/forms/src/App.js
@@ -1,20 +1,25 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Note from "./Note";
+import noteServices from "./services/notes";
 
-const App = (props) => {
-  const [notes, setNote] = useState(props.notes);
+const App = () => {
+  const [notes, setNote] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3002/notes/${id}`
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-    axios.put(url, changedNote).then(response => {
-      setNote(notes.map(n => n.id !== id ? n : response.data))
-    })
-  }
+    // const url = `http://localhost:3002/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+    noteServices.update(id, changedNote).then((response) => {
+      setNote(notes.map((note) => (note.id !== id ? note : response)));
+    });
+  };
+
+  useEffect(() => {
+    noteServices.getAll().then((note) => setNote(note));
+  }, []);
 
   const setFalseOrTrue = (e) => {
     e.preventDefault();
--- FSA/part2/forms/src/index.js
+++ FSA/part2/forms/src/index.js
@@ -1,11 +1,8 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import notservice from "./services/notes";
 
-import axios from 'axios'
 
-axios.get('http://localhost:3002/notes').then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App notes={notes} />
-  )
-})
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
