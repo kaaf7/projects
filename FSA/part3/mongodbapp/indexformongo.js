4265568218908b6c4a05615980db741e2d08a386
kaaf7 <amer.m.karim@gmail.com>
2022-09-26 20:09:09 +0200

    init

--- FSA/part3/mongodbapp/indexformongo.js
+++ FSA/part3/mongodbapp/indexformongo.js
@@ -1,49 +1,28 @@
+const PORT = process.env.PORT || 5001;
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
-const PORT = 5001;
-const cors = require("cors");
+const url = "";
+const mongoose = require("mongoose");
 
-
-
-app.use(express.json());
-app.use(cors());
-
-app.listen(PORT, () => {
-  console.log("error");
+const noteSchema = mongoose.noteSchema({
+  content: String,
+  important: Boolean,
+  date: Date,
 });
 
-
-
-
-
-const url = "mongodb+srv://newkarim:karim@cluster0.fdc02zk.mongodb.net/?retryWrites=true&w=majority"
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
+const Notes = mongoose.model("Note", noteSchema);
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log('connected')
-
-    const note = new Note({
-      content: 'HTML is Easy',
-      date: new Date(),
+  .then((results) => {
+    console.log("mongoose is connected");
+    const note = new Notes({
+      content: "new content",
       important: true,
-    })
-
-    return note.save()
+      date: new Date(),
+    });
+    return note.save();
   })
   .then(() => {
-    console.log('note saved!')
-    return mongoose.connection.close()
-  })
-  .catch((err) => console.log(err))
-
-
+    console.log("data is saved")
+    mongoose.connection.close();
+  });
